57a1f720d82c83d7e8a1517d77c1d2a60e113031
AbdulHadi117 <117abdulhadi@gmail.com>
2026-02-18 21:27:21 +0500

    Connect To NewsAPI and fetch & store articles

---
+++ project/app/services/news_service.py
@@ -0,0 +1,60 @@
+import requests
+from flask import current_app
+from app.models.article import Article
+from app import db
+from datetime import datetime
+
+class NewsService:
+
+    @staticmethod
+    def fetch_from_newsapi(topic):
+        url = "https://newsapi.org/v2/everything"
+
+        params = {
+            'q': topic,
+            'apiKey': current_app.config['NEWS_API_KEY'],
+            'language': 'en',
+            'pageSize': 10
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        articles = []
+
+        if data.get('status') == 'ok':
+            for item in data.get('articles', []):
+                article = {
+                    'title': item.get('title'),
+                    'description': item.get('description'),
+                    'source': item.get('source', {}).get('name'),
+                    'published_at': item.get('publishedAt')                    
+                }
+                articles.append(article)
+
+        return articles
+    
+    @staticmethod
+    def save_articles(topic, articles):
+        for item in articles:
+            if not item['title']:
+                continue
+
+            existing = Article.query.filter_by(
+                title=item['title'],
+            ).first()
+
+            if existing:
+                continue
+
+            article = Article(
+                title=item["title"],
+                description=item["description"],
+                source=item["source"],
+                topic=topic,
+                published_at=datetime.fromisoformat(
+                    item["published_at"].replace("Z", "+00:00")
+                ) if item["published_at"] else None
+            )
+            db.session.add(article)
+        db.session.commit()
