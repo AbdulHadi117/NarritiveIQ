4c5737722060be413759ba8aaebc61465e634aa8
AbdulHadi117 <117abdulhadi@gmail.com>
2026-02-18 21:28:42 +0500

    Update Routes to fetch articles and store in DB

--- project/app/routes/main_routes.py
+++ project/app/routes/main_routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from app.services.news_service import NewsService
+from app.models.article import Article
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
+@main.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    if request.method == "POST":
+        topic = request.form.get("topic")
+
+        articles = NewsService.fetch_from_newsapi(topic)
+        NewsService.save_articles(topic, articles)
+
+    stored_articles = Article.query.order_by(Article.created_at.desc()).all()
+
+    return render_template("index.html", articles=stored_articles)
