4211f8a46264e93a852d80c69f73e8617f417860
AbdulHadi117 <117abdulhadi@gmail.com>
2026-02-20 21:03:03 +0500

    Update routes to compute metrics

--- project/app/routes/main_routes.py
+++ project/app/routes/main_routes.py
@@ -1,19 +1,36 @@
 from flask import Blueprint, render_template, request
 from app.services.news_service import NewsService
 from app.models.article import Article
+from app.services.bias_service import BiasService
 
 main = Blueprint("main", __name__)
 
 @main.route("/", methods=["GET", "POST"])
 def home():
+
+    topic = None
+
     if request.method == "POST":
         topic = request.form.get("topic")
 
         articles_newsapi = NewsService.fetch_from_newsapi(topic)
         articles_gnews = NewsService.fetch_from_gnews(topic)
-        
-        all_articles = articles_newsapi + articles_gnews
-        NewsService.save_articles(topic, all_articles)
 
-    stored_articles = Article.query.order_by(Article.created_at.desc()).all()
-    return render_template("index.html", articles=stored_articles)
+        combined_articles = articles_newsapi + articles_gnews
+        NewsService.save_articles(topic, combined_articles)
+
+    stored_articles = Article.query.order_by(
+        Article.created_at.desc()
+    ).all()
+
+    source_metrics = BiasService.compute_source_metrics(stored_articles)
+    bias_index = BiasService.compute_bias_index(source_metrics)
+    polarization = BiasService.compute_polarization(source_metrics)
+
+    return render_template(
+        "index.html",
+        articles=stored_articles,
+        source_metrics=source_metrics,
+        bias_index=bias_index,
+        polarization=polarization
+    )
